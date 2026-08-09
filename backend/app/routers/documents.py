c1aa07b128f9bedfc24564fba284c282ae03945c
Tanishka Menon <menontanishka3112@gmail.com>
2026-08-09 19:01:12 +0530

    resolved gitignore tracking and finalize file upload endpoint

--- backend/app/routers/documents.py
+++ backend/app/routers/documents.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, UploadFile, File
 from app.models import Document,CreateDocument
+import os
 
 router = APIRouter()
+
+upload_folder = "app/uploads"
 
 documents=[]
 
@@ -42,3 +45,32 @@ def delete_Document_by_ID(id: int):
         status_code = status.HTTP_404_NOT_FOUND,
         detail = "Document Not Found !!!"
     )
+
+@router.post("/documents/upload")
+def upload_document(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Only pdf files allowed."
+        )
+
+    filename = file.filename
+    destination_path = os.path.join(upload_folder,filename)
+
+    counter = 1
+    base_name,extension = os.path.splitext(filename)
+
+    while os.path.exists(destination_path):
+        filename = f"{base_name}_{counter}{extension}"
+        destination_path = os.path.join(upload_folder,filename)
+        counter += 1
+
+    with open(destination_path, "wb") as buffer:
+        content = file.read()
+        buffer.write(content)
+
+    return {
+        "message": "File uploaded successfully",
+        "filename": filename,
+        "content_type": file.content_type
+    }
